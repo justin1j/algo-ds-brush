f317ef7aafcd7b5afd3a8d73e04b8f45298566b1
justin1j <wonjun110@gmail.com>
2020-03-28 17:09:10 -0700

    implemented events, sorting, validate bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,34 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+const Node = require('./node');
+
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
+
+const n = new Node(10);
+n.insert(5);
+n.insert(15);
+n.insert(0);
+n.insert(20);
+
+console.log(validate(n))
 
 module.exports = validate;
